99d4d70df1a7b161117ff16f6d986d14f0916370
DahaChikina5404 <valentina_kpss@mail.ru>
2023-11-12 21:16:55 +0700

    Переименование проекта

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <div className="py-10 bg-indigo-100 flex flex-col justify-between items-center gap-2">
-            <a href="#">example@mail.ru</a>
+            <p>example@mail.ru</p>
             <p>+7 (123) 456-77-89</p>
             <p>г. Новосибирск, Красный проспект, 50</p>
         </div>
