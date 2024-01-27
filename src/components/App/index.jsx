d55b89310da3458cdacdec37b2d16f1adf5e8b7c
DahaChikina5404 <valentina_kpss@mail.ru>
2024-01-27 23:46:47 +0700

    Добавлен фильтр по категории трат

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -10,16 +10,17 @@ function App() {
 
     const [expensesList, setExpensesList] = useState([]) // МАССИВ расходов пользователя
     const [errorModal, setErrorModal] = useState(false) // состояние ошибки
-    // const [filter, setFilter] = useState() // фильтр трат
+    const [selectedCategory, setSelectedCategory] = useState('all') // фильтр трат по категории
 
     // функция для добавления нового расхода (покупки) в список расходов
     const handleAddExpense = (expense) => {
 
-        if (!expense.cost || expense.cost <= 0) {
+        if (!expense.cost || expense.cost <= 0 || !expense.title) {
             setErrorModal(true)
             return
         } else {
-            setExpensesList(prevExpense => [...prevExpense, expense].sort((a, b) => new Date(a.startDate) - new Date(b.startDate)))
+            const expenseWithCat = {...expense, category: expense.title}
+            setExpensesList(prevExpense => [...prevExpense, expenseWithCat].sort((a, b) => new Date(a.startDate) - new Date(b.startDate)))
         }
     }
 
@@ -31,47 +32,47 @@ function App() {
         return expensesList.reduce((sum, expense) => sum + parseFloat(expense.cost), 0)
     }
     
+    const handleCategoryFilter = (category) => {  // обработчик для кнопки (фильтрация по категории)
+        setSelectedCategory(category)
+    }
     
-    //const categoryFilter = expensesList.filter
-   // expensesList.map //мы получили масссив трат и можем теперь их фильтровать!!!
+    let filteredExpenses
       
-    // function categoryFilter(currentCategory) { // Фильтр по категормм
-    //     if (currentCategory === 'all') {
-    //         setFilter(expenses)
-    //     } else {
-    //         const newCategory = [...expenses].filter((expense) => {
-    //             return expense.category === category
-    //         })
-    //         setFilter(newCategory)
-    //     }
-    // }
-
+    if (selectedCategory === 'all') {
+        filteredExpenses = expensesList
+    } else {
+        filteredExpenses = expensesList.filter((expense) => expense.category === selectedCategory || !expense.category)
+    }
 
     return (
-        <div className="content w-1/2 bg-slate-100 rounded shadow-md">
+        <div className="content md:w-1/2 bg-slate-100 rounded shadow-md">
             <h1 className="pb-10 text-center text-4xl font-semibold">Учёт расходов</h1>
             {errorModal && <ErrorModalWindow closeModalError={closeModalError} />}
             
             <ExpensesForm handleAddExpense={handleAddExpense} />
-            <ExpensesChart expensesList={expensesList} />
+            <ExpensesChart expensesList={filteredExpenses} />
 
             <p className="my-5 text-xl md:text-2xl">Общая сумма расходов: {calculateTotalExpense()} руб.</p>
 
-            <div className="flex flex-wrap flex-col md:flex-row justify-center md:items-center gap-2">
-                <Button title="Все расходы" data-category="all"/>
-                <Button title="Еда" data-category="Еда"/>
-                <Button title="Одежда" data-category="Одежда"/>
-                <Button title="Аренда" data-category="Аренда"/>
-                <Button title="Образование" data-category="Образование"/>
-                <Button title="Развлечения" data-category="Развлечения"/>
-                <Button title="Путешествия" data-category="Путешествия"/>
+            <div className="my-5 flex flex-wrap flex-col md:flex-row justify-center md:items-center gap-2">
+                <Button title="Все расходы" onClick={() => handleCategoryFilter('all')} />
+                <Button title='Еда' onClick={() => handleCategoryFilter('Еда')} />
+                <Button title='Одежда' onClick={() => handleCategoryFilter('Одежда')} />
+                <Button title='Аренда' onClick={() => handleCategoryFilter('Аренда')} />
+                <Button title='Образование' onClick={() => handleCategoryFilter('Образование')} />
+                <Button title='Развлечения' onClick={() => handleCategoryFilter('Развлечения')} />
+                <Button title='Путешествия' onClick={() => handleCategoryFilter('Путешествия')} />
             </div>
 
-            {expensesList.map((expense, index) => {
-                return (
-                    <Costs key={index} expense={expense} />
-                )
-            })}
+            {filteredExpenses.length === 0 ? ( // условие если в выбранной категории нет трат
+                <p className="my-5 text-center text-lg md:text-xl">Нет трат в выбранной категории!</p>
+            ) : (
+                filteredExpenses.map((expense, index) => {
+                    return (
+                        <Costs key={index} expense={expense} />
+                    )
+                })
+            )}
         </div>
     )
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,9 @@
-function Button( { title }) {
+function Button( { title, onClick }) {
     return (
-        <button className="py-1 px-3 text-white bg-indigo-600 rounded hover:bg-indigo-800 transition-all duration-300">
+        <button 
+            className="py-1 px-3 text-white bg-indigo-600 rounded hover:bg-indigo-800 transition-all duration-300"
+            onClick={onClick}
+        >
             {title}
         </button>
     )
