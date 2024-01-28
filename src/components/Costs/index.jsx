4a08e9fba0739340e5119994452144224a117a12
DahaChikina5404 <valentina_kpss@mail.ru>
2024-01-29 00:18:16 +0700

    Отредактирована вёрстка

--- src/components/Costs/index.jsx
+++ src/components/Costs/index.jsx
@@ -8,7 +8,7 @@ function Costs({ expense }) {
     return (
         <div className="pt-6 text-sm md:text-xl flex gap-5 justify-between items-center border-b-2 border-solid border-indigo-200">
             <div className="flex flex-col gap-1">
-                <p className="text-lg border-none bg-indigo-200 rounded-md">{format(formatDate, 'dd MMMM yyyy', {locale: ru})}</p>
+                <p className="text-sm md:text-lg border-none bg-indigo-200 rounded-md">{format(formatDate, 'dd MMMM yyyy', {locale: ru})}</p>
                 <p>{expense.title}</p>
             </div>
            
